e719b659a9bcf835c23ed8f19428299f4397843f
Farzana Karim <farzana.mkarim@gmail.com>
2020-07-24 03:44:16 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+
+
+
+def using_concat(array, new_array)
+   array.concat(new_array)
+end
+
+
+
+def using_insert(array, element)
+   array.insert = "cat"
+end
+
+array = ["Tom", "Sally", "Tom"]
+def using_uniq(array)
+   array.uniq
+
+end
+
+
+def using_flatten(array[1, 3])
+  array.flatten
+
+end
